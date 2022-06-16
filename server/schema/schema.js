4d6ab98f1cd3d55a06614a03527fb4e4fe159bf1
yasirghouri <yasir.ag7@gmail.com>
2022-06-16 20:53:56 +0500

    implemented graphql to get data from mongoDB

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
-const { clients, projects } = require("./sampleData");
+const Client = require("./models/Client");
+const Project = require("./models/Project");
 
 const {
   GraphQLObjectType,
@@ -19,7 +20,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
       },
     },
   }),
@@ -43,26 +44,26 @@ const RootQuery = new GraphQLObjectType({
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
   },
