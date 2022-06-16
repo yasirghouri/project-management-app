92f8bf32cce3fb78361bfc2719434e5f7684faf5
yasirghouri <yasir.ag7@gmail.com>
2022-06-16 22:56:49 +0500

    completed Clients component with spinner and added delete client mutation

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,26 +1,18 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_CLIENTS = gql`
-    query clients {
-        clients {
-            name
-            id
-            email
-            phone
-          }
-    }
-`
+import { useQuery } from "@apollo/client";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import ClientRow from "./ClientRow";
+import Spinner from "./Spinner";
 
 const Clients = () => {
-    const { loading, error, data } = useQuery(GET_CLIENTS);
+  const { loading, error, data } = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Spinner />;
   if (error) return <p>Something Went Wrong</p>;
 
   return (
     <>
       {!loading && !error && (
-        <table className='table table-hover mt-3'>
+        <table className="table table-hover mt-3">
           <thead>
             <tr>
               <th>Name</th>
@@ -30,19 +22,14 @@ const Clients = () => {
             </tr>
           </thead>
           <tbody>
-            {data.clients.map((client) => (
-              <tr>
-              <td>{client.name}</td>
-              <td>{client.email}</td>
-              <td>{client.phone}</td>
-
-            </tr>
+            {data.clients.map((client, idx) => (
+              <ClientRow key={idx} client={client} />
             ))}
           </tbody>
         </table>
       )}
     </>
   );
-}
+};
 
 export default Clients;
